11937f5425316d0bf80b030d461c9f8362177809
tanbi <tanbi@DESKTOP-ALP9I4R.lan>
2024-12-09 11:37:30 -0500

    addition two number

---
+++ src/plus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class plus {
+public static void main(String[]args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("ENTER THE FIRST NUMBER:");
+int num1= scanner.nextInt();
+System.out.print("Enter the second number:");
+int num2= scanner.nextInt();
+int result=num1 - num2;
+System.out.println("the subs of"num1"and" + num2 + "is:" + result);
+scanner.close();
+}
+}
+
+
+
+
+
+
+
+//user inter a first name : 8 /random num
+//userinter 2nd number :n 6 
+// result : 14
