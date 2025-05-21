a145139faccbb4fb925b9053c1bb633b06973b92
namir <namir@Umaila.lan>
2025-05-21 15:46:55 -0400

    first push

--- src/plus.java
+++ src/plus.java
@@ -8,7 +8,7 @@ int num1= scanner.nextInt();
 System.out.print("Enter the second number:");
 int num2= scanner.nextInt();
 int result=num1 - num2;
-System.out.println("the subs of"num1"and" + num2 + "is:" + result);
+System.out.println("the subs of" +num1 +"and" + num2 + "is:" + result);
 scanner.close();
 }
 }
